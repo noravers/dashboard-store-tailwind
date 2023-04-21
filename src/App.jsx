b385675f2cdb58d4f4c27b476f68a143d0637596
noravers <gia.drago4@gmail.com>
2023-04-21 15:41:22 -0600

    Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {RiMenu3Fill, RiUser3Line, RiAddFill, RiPieChartLine, RiCloseLine} from 'react-icons/ri'
+import {RiMenu3Fill, RiUser3Line, RiAddFill, RiPieChartLine, RiCloseLine, RiSearchLine} from 'react-icons/ri'
 import Sidebar from "./components/shared/Sidebar"
 
 import { useState } from 'react'
@@ -34,9 +34,23 @@ function App() {
         }
           </button>
     </nav>
-    <main className='pl-28 grid grid-cols-1 lg:grid-cols-8'>
-      <div className='lg:col-span-6'>Hola</div>
-      <div className='lg:col-span-2'>Hola 2</div>
+    <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8'>
+      <div className='lg:col-span-6 p-2'>
+        <header className=''>
+          <div className='flex flex-col gap-4'>
+            <div className="">
+              <h1 className='text-2xl text-gray-300'>Jeager Resto</h1>
+              <p className='text-gray-500'>April 20th 2023</p>
+            </div>
+            <form className='w-full relative'>
+              <RiSearchLine className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-300'/>
+              <input type="text" placeholder='Search for food, cofee, etc' className='bg-[#1f1d2b] w-full py-2 pr-4 pl-10 rounded-lg text-gray-300 outline-none' />
+            </form>
+          </div>
+
+        </header>
+     </div>
+      <div className='bg-red-100 lg:col-span-2 fixed right-0 lg:static'>Carrito</div>
     </main>
    </div>
   )
